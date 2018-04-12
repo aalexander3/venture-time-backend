54145d5db99afce8fd06245e24bf80bc715b65dd
aalexander3 <aralx73@gmail.com>
2018-04-12 09:40:17 -0400

    Seeds and stuff

--- venture-time/db/seeds.rb
+++ venture-time/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+arren = Investor.create(name: "Arren Alexander", mission: "Help exciting new companies grow", description: "Small investing firm looking for new investment opportunities", logo: 'https://images.unsplash.com/photo-1504670073073-6123e39e0754?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=23c233a430f2ca6be8d78f8608b416d5&auto=format&fit=crop&w=1050&q=80', url: 'https://www.linkedin.com/in/arren-alexander-10a361124', funds_to_invest: 1200000)
+ryan = Investor.create(name: "Ryan Farney", mission: "Gettin this money", description: "Big investing firm looking make cash baby", logo: 'https://images.unsplash.com/photo-1518169640858-0d622b058e5c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5ffa10ff5bac3a77cb7de6e4e7164787&auto=format&fit=crop&w=1083&q=80', url: 'https://www.linkedin.com/in/ryanfarney3/', funds_to_invest: 500000)
+
+company1 = StartUp.create(name: "Flatiron School", mission: "Learn Love Code", description: "Creating the next tier of code masters", field: "Technology", logo: 'https://cdn.dribbble.com/users/8894/screenshots/3370036/flatiron-school_gif.gif', url: 'https://flatironschool.com/', funds_needed: 25000)
+company1 = StartUp.create(name: "Pete's Cafe", mission: "Roasting and toasting", description: "Creating master blends using the finest locally sourced coffee beans. Sustainable, fair-trade, organic. Located in Brooklyn", field: "Technology", logo: 'https://images.unsplash.com/photo-1505356988697-dc2fee0626f4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1a38196ff97e883d49c72bdd969bef6a&auto=format&fit=crop&w=1050&q=80', url: 'https://petescafe.com/', funds_needed: 30000)
