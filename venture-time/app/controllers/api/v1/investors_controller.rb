a1806931d8c754708463253359aa6d76f97c204f
aalexander3 <aralx73@gmail.com>
2018-04-17 18:23:23 -0400

    half done with create

--- venture-time/app/controllers/api/v1/investors_controller.rb
+++ venture-time/app/controllers/api/v1/investors_controller.rb
@@ -6,6 +6,18 @@ module Api
         render json: Investor.all
       end
 
+      def create
+        @investor = Investor.new(investor_params)
+        if @investor.save
+          render json: Investor.all
+        end
+      end
+
+      private
+      def investor_params
+        params.require(:investor).permit(:mission, :description, :interests, :logo, :url, :username, :name)
+      end
+
     end
   end
 end
--- venture-time/app/controllers/api/v1/start_ups_controller.rb
+++ venture-time/app/controllers/api/v1/start_ups_controller.rb
@@ -6,7 +6,18 @@ module Api
         render json: StartUp.all
       end
 
-      
+      def create
+        @startup = @startup.new(start_up_params)
+        if @startup.save
+          render json: StartUp.all
+        end
+      end
+
+      private
+      def start_up_params
+        params.require(:startup).permit(:password, :mission, :description, :interests, :logo, :url, :username, :name)
+      end
+
     end
   end
 end
