17bbd6b7fc1e7be4dd3c50e3487cf0db73e6d3f7
aalexander3 <aralx73@gmail.com>
2018-09-17 17:14:03 -0400

    secure password

--- app/models/start_up.rb
+++ app/models/start_up.rb
@@ -1,6 +1,6 @@
 class StartUp < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  # has_secure_password
+  has_secure_password
 
   has_many :start_up_investors
   has_many :investors, through: :start_up_investors
